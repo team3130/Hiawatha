eabd0aa2b3b3f200b96fb8a6953e79fac2dcd778
Duckfan77 <cwenthin@yahoo.com>
2017-01-17 20:29:33 -0600

    Revert "Revert "Created Commands for Spinning the climber up and down""
    This reverts commit 57ed6416c550e9b85378d02f5f76676b37ac3cf9.

---
+++ src/org/usfirst/frc/team3130/robot/commands/ClimbDown.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team3130.robot.commands;
+
+import org.usfirst.frc.team3130.robot.subsystems.Climber;
+
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ClimbDown extends Command {
+
+    public ClimbDown() {
+        requires(Climber.GetInstance());
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Climber.Climb(Preferences.getInstance().getDouble("Climber Down Speed", -1.0));
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Climber.Climb(0.0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    end();
+    }
+}
