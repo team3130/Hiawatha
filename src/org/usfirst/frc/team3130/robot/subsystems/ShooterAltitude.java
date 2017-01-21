8ef3714b1dc94348ebde38337f81915ff21e282f
LoopedBard3 <pbibus@hotmail.com>
2017-01-20 22:31:45 -0600

    Shooter Altitude changes
    Added ShooterAltitudeAdjust and added some basics to ShooterAltitude

--- src/org/usfirst/frc/team3130/robot/subsystems/ShooterAltitude.java
+++ src/org/usfirst/frc/team3130/robot/subsystems/ShooterAltitude.java
@@ -3,35 +3,77 @@ package org.usfirst.frc.team3130.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.*;
 import org.usfirst.frc.team3130.robot.RobotMap;
+import org.usfirst.frc.team3130.robot.commands.ShooterAltitudeAdjust;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 
 public class ShooterAltitude extends Subsystem {
+	
+	//Define constants
+	private static final double RATIO_TICKSPERINCH = 7*71*Math.PI*(1.9+.05);	//Winch Diamater ~1.9, Rope Radius ~.05
+	public static final double TOP_ALT = 26;
 
 	//Instance Handling
     private static ShooterAltitude m_pInstance;
-    /**
-     * A system for getting an instance of this class.
-     * The function provides a method by which the class is setup as a singleton
-     * with only a single copy of it existing in memory.
-     * <p> It will return a reference to the class, which is shared amoungst all callers of GetInstance()
-     * 
-     * @return the reference to the class refered to in GetInstance. In this case, ShooterAltitude.
-     */
     public static ShooterAltitude GetInstance()
     {
     	if(m_pInstance == null) m_pInstance = new ShooterAltitude();
     	return m_pInstance;
     }
     
-    CANTalon m_altitudeControler;
+    //Define Objects
+    private static CANTalon m_altitudeController;
+    
+    
+    //Define basic memory types
+    
+    
     
     private ShooterAltitude() {
-    	m_altitudeControler = new CANTalon(RobotMap.CAN_SHOOTERALTITUDE);
+    	//Initialize Basic data types
+    	
+    	
+    	//Set up Talon
+    	m_altitudeController = new CANTalon(RobotMap.CAN_SHOOTERALTITUDE);
+    	m_altitudeController.changeControlMode(TalonControlMode.Position);
+    	m_altitudeController.enableForwardSoftLimit(true);
+    	m_altitudeController.setForwardSoftLimit(TOP_ALT);
+    	
+    	
     }
 
+    //Set the default command to ShooterAltitudeAdjust
     public void initDefaultCommand() {
-        
+    	setDefaultCommand(new ShooterAltitudeAdjust());
     }
+    
+  //Getters
+    //Check if shooter is at the bottom altitude
+    public static boolean isAtBottom()
+    {
+    	return m_altitudeController.isRevLimitSwitchClosed();
+    }
+    
+    //Get position of the canTalon
+    public static double getAltitude(){
+    	return m_altitudeController.getPosition();
+    }
+    
+    //Make Talon go to a certain position 
+    public static void setAltitude(double goal){
+    	
+    }
+    
+    //Increase the Altitude by set amount
+    public static void increaseAltitude(double increase){
+    	setAltitude(getAltitude() + increase);
+    }
+    
+    //Decrease the Altitude by set amount
+    public static void decreaseAltitude(double decrease){
+    	setAltitude(getAltitude() - decrease);
+    }
+    
 }
 
