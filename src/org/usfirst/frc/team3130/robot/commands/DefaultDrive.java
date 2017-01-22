2cd2dc9014bffb463b7a8fcd9ff3939d9be7b42b
Duckfan77 <cwenthin@yahoo.com>
2017-01-21 18:37:43 -0600

    Implemented DefaultDrive.java
    Implemented DefaultDrive.java with the file from Greenwood, as the drive
    system from the previous year is fully acceptable for driving this
    robot.

--- src/org/usfirst/frc/team3130/robot/commands/DefaultDrive.java
+++ src/org/usfirst/frc/team3130/robot/commands/DefaultDrive.java
@@ -1,8 +1,10 @@
+
 package org.usfirst.frc.team3130.robot.commands;
 
-import org.usfirst.frc.team3130.robot.subsystems.Chassis;
-
 import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team3130.robot.OI;
+import org.usfirst.frc.team3130.robot.subsystems.Chassis;
 
 /**
  *
@@ -10,16 +12,22 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DefaultDrive extends Command {
 
     public DefaultDrive() {
-        // Use requires() here to declare subsystem dependencies
         requires(Chassis.GetInstance());
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Chassis.ReleaseAngle();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double moveSpeed = -OI.stickL.getY();
+    	double turnSpeed = -OI.stickR.getX();
+    	double turnThrottle = (-0.5 * OI.stickR.getRawAxis(3)) + 0.5;
+    	
+    	//Explicitly turning on Quadratic inputs for drivers, as all other systems will use nonQuadratic
+    	Chassis.DriveArcade(moveSpeed, turnSpeed * turnThrottle, true);
     }
 
     // Make this return true when this Command no longer needs to run execute()
