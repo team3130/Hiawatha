efa664f29d5d20d8dbbaf78c59a81e6e9185f8fc
Duckfan77 <cwenthin@yahoo.com>
2017-01-22 16:14:58 -0600

    Added the GetInstance to the new Subsystems
    Created a GearGrabber Subsystem, and added GetInstance to GearGrabber
    and JetsonInterface.

--- src/org/usfirst/frc/team3130/robot/subsystems/GearGrabber.java
+++ src/org/usfirst/frc/team3130/robot/subsystems/GearGrabber.java
@@ -5,21 +5,21 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class JetsonInterface extends Subsystem {
+public class GearGrabber extends Subsystem {
 
 	//Instance Handling
-    private static JetsonInterface m_pInstance;
+    private static GearGrabber m_pInstance;
     /**
      * A system for getting an instance of this class.
      * The function provides a method by which the class is setup as a singleton
      * with only a single copy of it existing in memory.
      * <p> It will return a reference to the class, which is shared amoungst all callers of GetInstance()
      * 
-     * @return the reference to the class refered to in GetInstance. In this case, JetsonInterface.
+     * @return the reference to the class refered to in GetInstance. In this case, GearGrabber.
      */
-    public static JetsonInterface GetInstance()
+    public static GearGrabber GetInstance()
     {
-    	if(m_pInstance == null) m_pInstance = new JetsonInterface();
+    	if(m_pInstance == null) m_pInstance = new GearGrabber();
     	return m_pInstance;
     }
     
