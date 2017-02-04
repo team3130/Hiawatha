eafd987a2aa2e24c2c061319c9f14a2508e2864e
Duckfan77 <cwenthin@yahoo.com>
2017-02-03 21:40:00 -0600

    Implemented the GearGrabber Subsystem
    Defined 3 solenoids that actuate the movement axis of the GearGrabber,
    those being pinch, lift, and door. Each one has an individual function
    that allows the setting of the state of each solenoid.

--- src/org/usfirst/frc/team3130/robot/subsystems/GearGrabber.java
+++ src/org/usfirst/frc/team3130/robot/subsystems/GearGrabber.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team3130.robot.subsystems;
 
+import org.usfirst.frc.team3130.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -23,10 +26,56 @@ public class GearGrabber extends Subsystem {
     	return m_pInstance;
     }
     
+    private static Solenoid m_pinchSolenoid;
+    private static Solenoid m_doorSolenoid;
+    private static Solenoid m_liftSolenoid;
+    
+    private GearGrabber()
+    {
+    	m_pinchSolenoid = new Solenoid(RobotMap.PNM_GEARPINCH);
+    	m_doorSolenoid  = new Solenoid(RobotMap.PNM_GEARDOOR);
+    	m_liftSolenoid = new Solenoid(RobotMap.PNM_GEARLIFT);
+    }
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    /**
+     * Actuates the gear pinch system
+     * <p>Controls the movement of the gear pinch cylinder. The value passed to the function changes the state of the cylinder, 
+     * with true extending the cylinder, and false retracting it.
+     * </p>
+     * @param extend to extend the cylinder or not, true extends, false retracts
+     */
+    public static void pinchActuate(boolean actuate)
+    {
+    	m_pinchSolenoid.set(actuate);
+    }
+    
+    /**
+     * Actuates the gear door system
+     * <p>Controls the movement of the gear door cylinder. The value passed to the function changes the state of the cylinder, 
+     * with true extending the cylinder, and false retracting it.
+     * </p>
+     * @param extend to extend the cylinder or not, true extends, false retracts
+     */
+    public static void doorActuate(boolean actuate)
+    {
+    	m_doorSolenoid.set(actuate);
+    }
+    
+    /**
+     * Actuates the gear lift system
+     * <p>Controls the movement of the gear lift cylinder. The value passed to the function changes the state of the cylinder, 
+     * with true extending the cylinder, and false retracting it.
+     * </p>
+     * @param extend to extend the cylinder or not, true extends, false retracts
+     */
+    public static void liftActuate(boolean actuate)
+    {
+    	m_liftSolenoid.set(actuate);
     }
 }
 
