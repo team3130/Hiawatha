30471f2e2f28012cc49e8fe3df2943161d669a9f
Duckfan77 <cwenthin@yahoo.com>
2017-02-11 00:39:21 -0600

    Added an AutoTurn command
    Added a command to turn in place to a relative angle.

---
+++ src/org/usfirst/frc/team3130/robot/autoCommands/AutoTurn.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team3130.robot.autoCommands;
+
+import org.usfirst.frc.team3130.robot.subsystems.Chassis;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutoTurn extends Command {
+
+	private double m_angle;
+	
+    public AutoTurn() {
+        requires(Chassis.GetInstance());
+    }
+
+    public void SetParam(double angle)
+    {
+    	m_angle = angle;
+    }
+    
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Chassis.HoldAngle(m_angle);
+    	Chassis.GetInstance().setAbsoluteTolerance(0.5);
+    	Chassis.DriveStraight(0);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Chassis.GetInstance().onTarget();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Chassis.ReleaseAngle();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
