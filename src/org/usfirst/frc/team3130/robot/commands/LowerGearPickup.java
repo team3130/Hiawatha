3bf0de69c2a894ef9a38fcff7e48c02e93460e00
Duckfan77 <cwenthin@yahoo.com>
2017-03-15 17:59:04 -0500

    Added Gear Motor Speed to Preferences

--- src/org/usfirst/frc/team3130/robot/commands/LowerGearPickup.java
+++ src/org/usfirst/frc/team3130/robot/commands/LowerGearPickup.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3130.robot.commands;
 import org.usfirst.frc.team3130.robot.Robot;
 import org.usfirst.frc.team3130.robot.autoCommands.AutoBasicActuate;
 
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -20,7 +21,7 @@ public class LowerGearPickup extends CommandGroup {
     	backToDefault = new PickupGear();
     	
     	addParallel(new AutoBasicActuate(Robot.bcGearPinch, false), 0.1);
-    	addParallel(new BasicSpinMotor(Robot.btGearBar, .8));	//TODO: Determine Polarity
+    	addParallel(new BasicSpinMotor(Robot.btGearBar, Preferences.getInstance().getDouble("Gear Motor PercentVBus", .8)));	//TODO: Determine Polarity
     	addSequential(new AutoBasicActuate(Robot.bcGearLift, true), 0.1);
     }
     
