8c00078876138bf226e5b464a12b7bf1f1a7175e
Duckfan77 <cwenthin@yahoo.com>
2017-04-08 08:06:03 -0600

    New Wheel Diameter

--- src/org/usfirst/frc/team3130/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team3130/robot/subsystems/Chassis.java
@@ -50,7 +50,7 @@ public class Chassis extends PIDSubsystem {
 
 	public static final double robotWidth = 26.0;
 
-	public static final double InchesPerRev = Preferences.getInstance().getDouble("Wheel Diameter", 3.720) * Math.PI;
+	public static final double InchesPerRev = Preferences.getInstance().getDouble("Wheel Diameter", 3.697) * Math.PI;
 
 	
 	//PID Preferences Defaults
