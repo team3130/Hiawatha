7014b2255c377610f72f7a87e1bc7698260a42c8
Ashley Holton <ashleyholton2@hotmail.com>
2017-09-15 10:08:56 -0500

    Added turn after hopper back in
    If turret (and phone) both facing hopper, won't see the boiler targets
    at all and will never shoot. By turning a bit towards the goal, the
    phone should be able to pick up on the targets and aim the turret.

--- src/org/usfirst/frc/team3130/robot/autoCommands/ShootAfterHopper.java
+++ src/org/usfirst/frc/team3130/robot/autoCommands/ShootAfterHopper.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team3130.robot.autoCommands;
 
+import org.usfirst.frc.team3130.robot.OI;
 import org.usfirst.frc.team3130.robot.commands.TurretAim;
 import org.usfirst.frc.team3130.robot.subsystems.Chassis;
 import org.usfirst.frc.team3130.robot.subsystems.TurretAngle;
@@ -16,7 +17,9 @@ public class ShootAfterHopper extends CommandGroup {
 	private AutoDriveStraightToPoint	drive_backFromHopper;
 	private TurretAim					shoot_aimAndShoot;
 	private AutoDelay					delay_Generic1;
+	private AutoDelay					delay_Generic2;
 	private AutoDelay					delay_waitData;
+	private AutoTurn					turnToBoiler;
 	
     public ShootAfterHopper() {
 		requires(Chassis.GetInstance());
@@ -24,19 +27,29 @@ public class ShootAfterHopper extends CommandGroup {
 		requires(TurretFlywheel.GetInstance());
 		
 		drive_backFromHopper = new AutoDriveStraightToPoint();
-		shoot_aimAndShoot = new TurretAim();
-		delay_Generic1 = new AutoDelay();
 		delay_waitData = new AutoDelay();
+		turnToBoiler = new AutoTurn();
+		delay_Generic1 = new AutoDelay();
+		delay_Generic2 = new AutoDelay();
+		shoot_aimAndShoot = new TurretAim();
 
 		addSequential(delay_Generic1, 1.5);
 		addSequential(drive_backFromHopper, 1);
 		addSequential(delay_waitData, 1);
+		addSequential(turnToBoiler);
+		addSequential(delay_Generic2, 1);
 		addParallel(shoot_aimAndShoot);
     }
     
     @Override
     protected void initialize()
     {
+    	if(OI.fieldSide.getSelected() == "Red") {
+    		turnToBoiler.SetParam(Preferences.getInstance().getDouble("Auto Shoot Turn Angle", 45));
+    	}
+    	else {
+    		turnToBoiler.SetParam(Preferences.getInstance().getDouble("Auto Shoot Turn Angle", -45));
+    	}
     	
     	drive_backFromHopper.SetParam(
     			Preferences.getInstance().getDouble("Drive Back Hopper Dist", 24), 
